01a55103f36c54fac2596bed543f7878f2916d08
Arieu Sala <blognini.ariel.universidad@outlook.com>
2023-11-01 11:04:50 -0300

    Primer Commit OK.

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ function App() {
       <header className="App-header">
         <p>
           <code>USUARIO </code> 
-          <input className="usuario" type="text" name="buscar"></input> 
+          <input className="usuario" type="text" name="usuario"></input> 
           <code> PASSWORD </code> 
-          <input className="password" type="text" name="buscar"></input>
+          <input className="password" type="text" name="password"></input>
           <button onClick={pausar}>Login</button>
 
           <button>HOLA</button>
